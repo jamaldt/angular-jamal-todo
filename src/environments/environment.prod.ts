4ed34d7b0638f4959eff5818f915156ababb0891
jamal_dt <carldeltoro@gmail.com>
2019-05-22 16:38:50 -0400

    agregando configuracion netlify

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBAGkdp7JGSVAN4I8ualM8oUFK5m3lEles",
+    authDomain: "todo-jamal-app.firebaseapp.com",
+    databaseURL: "https://todo-jamal-app.firebaseio.com",
+    projectId: "todo-jamal-app",
+    storageBucket: "todo-jamal-app.appspot.com",
+    messagingSenderId: "728324836069",
+    appId: "1:728324836069:web:f1e79657569261d7"
+  }
 };
